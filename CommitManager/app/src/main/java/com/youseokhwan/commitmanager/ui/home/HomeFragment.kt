b40b61094e24c617c1668cc19bb004c2f756d1a4
youseokhwan <tjrghks115@naver.com>
2020-04-05 23:18:15 +0900

    HomeFragment UI 배치

--- CommitManager/app/src/main/java/com/youseokhwan/commitmanager/ui/home/HomeFragment.kt
+++ CommitManager/app/src/main/java/com/youseokhwan/commitmanager/ui/home/HomeFragment.kt
@@ -18,18 +18,14 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.HomeFragment_TextView_Temp)
+
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
         })
+
         return root
     }
 }
