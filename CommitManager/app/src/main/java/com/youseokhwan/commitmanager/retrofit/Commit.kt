7d6041054e7b5780b2f476c2374b9f9beec73732
kimkyunghoon <byul2sol@naver.com>
2020-05-04 12:07:45 +0900

    /commit dto 수정

--- CommitManager/app/src/main/java/com/youseokhwan/commitmanager/retrofit/Commit.kt
+++ CommitManager/app/src/main/java/com/youseokhwan/commitmanager/retrofit/Commit.kt
@@ -4,8 +4,12 @@ package com.youseokhwan.commitmanager.retrofit
  * Commit DTO
  * @property count 오늘 커밋한 횟수
  * @property lastCommit 최종 커밋 시간
+ * @property repository 최종 커밋 레포지토리
+ * @property msg 최종 커밋 메시지
  */
 data class Commit (
     val count: Int,
-    val lastCommit: String
+    val lastCommit: String,
+    val repository: String,
+    val msg: String
 )
